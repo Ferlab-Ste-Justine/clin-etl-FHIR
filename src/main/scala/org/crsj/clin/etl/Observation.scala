ce81db149f4e5573a930674683a10bc5954321d9
Francois Lachance <latch2112@gmail.com>
2019-06-26 16:44:53 -0400

    Update ETL

--- src/main/scala/org/crsj/clin/etl/Observation.scala
+++ src/main/scala/org/crsj/clin/etl/Observation.scala
@@ -19,7 +19,7 @@ object Observation {
     import spark.implicits._
     DataFrameUtils.load(s"$base/obs.ndjson", $"id", $"status", $"code", $"subject", $"effective",
       phenotypes($"value.CodeableConcept.coding") as "phenotype", $"note",
-      expr("interpretation[0].coding[0].code") as "display"
+      expr("interpretation[0].coding[0].code") as "observed"
     )
 
 
