244336a0cec89a332e5a9a28e3b3295cf0b21ca3
Francois Lachance <latch2112@gmail.com>
2019-06-26 15:35:47 -0400

    Update ETL

--- src/main/scala/org/crsj/clin/etl/ServiceRequest.scala
+++ src/main/scala/org/crsj/clin/etl/ServiceRequest.scala
@@ -21,7 +21,7 @@ object ServiceRequest {
       //refGetter(expr("extension.valueReference.reference")) as "ci_ref")
     expr("extension[0].valueReference.reference").substr(20,7) as "ci_ref")
 
-    val clinicalImpressions = DataFrameUtils.load(s"$base/ci.ndjson", $"id" as "ci_id", $"subject", $"status", $"effective")
+    val clinicalImpressions = DataFrameUtils.load(s"$base/ci.ndjson", $"id" as "ci_id", $"subject" as "ci_subj", $"status" as "ci_status", $"effective" as "ci_effective")
 
     val serviceRequestWithClinicalImpression = sr.select($"id", $"status", $"intent", $"authoredOn", $"code", $"subject", $"specimen", $"ci_ref")
       .join(clinicalImpressions.select("*"), $"ci_ref" === $"ci_id")
