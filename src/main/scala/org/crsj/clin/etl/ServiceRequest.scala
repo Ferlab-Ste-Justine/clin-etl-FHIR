b02f32b075e5e7f1da073b3c4b1611096b1e15ba
Francois Lachance <latch2112@gmail.com>
2019-06-26 14:44:30 -0400

    Update ETL

--- src/main/scala/org/crsj/clin/etl/ServiceRequest.scala
+++ src/main/scala/org/crsj/clin/etl/ServiceRequest.scala
@@ -43,6 +43,6 @@ object ServiceRequest {
     DataFrameUtils.load(s"$base/sr.ndjson",
       $"id", $"status", $"intent", $"authoredOn", $"code", $"subject", $"specimen",
       //refGetter(expr("extension.valueReference.reference")) as "ci_ref")
-    $"extension.valueReference.reference" as "ci_ref")
+    $"extension[0].valueReference.reference".substr(18,7) as "ci_ref")
   }
 }
