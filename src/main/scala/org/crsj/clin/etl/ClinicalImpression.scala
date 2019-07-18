3cc656cde70e3ec2f7b8b535b1521aa02a748955
Francois Lachance <latch2112@gmail.com>
2019-07-18 16:36:19 -0400

    Update ETL, CI with item

--- src/main/scala/org/crsj/clin/etl/ClinicalImpression.scala
+++ src/main/scala/org/crsj/clin/etl/ClinicalImpression.scala
@@ -6,10 +6,12 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 object ClinicalImpression {
   def load(base: String, practitionerWithRolesAndOrg: sql.DataFrame)(implicit spark: SparkSession): DataFrame = {
     import spark.implicits._
-    val clinicalImpression = DataFrameUtils.load(s"$base/ci.ndjson", $"id", $"subject", $"status", $"effective", $"extension.valueAge.value" (0) as "runtimePatientAge", $"assessor.id" as "assessor_id")
+    val clinicalImpression = DataFrameUtils.load(s"$base/ci.ndjson", $"id", $"subject",
+      $"status", $"effective", $"extension.valueAge.value" (0) as "runtimePatientAge", $"assessor.id" as "assessor_id",
+      $"investigation.item.uri" as "iiu")
 
     val clinicalImpressionWithAssessor = clinicalImpression
-      .select($"id", $"subject", $"status", $"effective" as "ci_consultation_date", $"runtimePatientAge", $"assessor_id")
+      .select($"id", $"subject", $"status", $"effective" as "ci_consultation_date", $"runtimePatientAge", $"assessor_id", $"iiu")
       .join(practitionerWithRolesAndOrg
       .select($"role_id", $"name" as "assessor_name", $"org_name" as "assessor_org_name"), $"assessor_id" === $"role_id")
     clinicalImpressionWithAssessor
