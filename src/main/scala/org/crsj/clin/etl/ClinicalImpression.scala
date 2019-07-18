49dc5c7be93f352b2d24302ed12f1f1839fd518c
Francois Lachance <latch2112@gmail.com>
2019-07-18 11:44:44 -0400

    Update ETL, CI with assessor

--- src/main/scala/org/crsj/clin/etl/ClinicalImpression.scala
+++ src/main/scala/org/crsj/clin/etl/ClinicalImpression.scala
@@ -1,10 +1,12 @@
 package org.crsj.clin.etl
 
+import org.apache.spark.sql
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object ClinicalImpression {
-  def load(base: String)(implicit spark: SparkSession): DataFrame = {
+  def load(base: String, practitionerWithRolesAndOrg: sql.DataFrame)(implicit spark: SparkSession): DataFrame = {
     import spark.implicits._
-    DataFrameUtils.load(s"$base/ci.ndjson", $"id", $"subject", $"status", $"effective", $"extension.valueAge.value" (0) as "runtimePatientAge", $"assessor")
+    val clinicalImpression = DataFrameUtils.load(s"$base/ci.ndjson", $"id", $"subject", $"status", $"effective", $"extension.valueAge.value" (0) as "runtimePatientAge", $"assessor.id" as "assessor_id")
+    clinicalImpression
   }
 }
--- src/main/scala/org/crsj/clin/etl/ServiceRequest.scala
+++ src/main/scala/org/crsj/clin/etl/ServiceRequest.scala
@@ -10,18 +10,20 @@ object ServiceRequest {
 
 
 
-  def load(base: String, practitionerWithRoles: sql.DataFrame, clinicalImpressions: sql.DataFrame)(implicit spark: SparkSession): DataFrame = {
+  def load(base: String, practitionerWithRolesAndOrg: sql.DataFrame, clinicalImpressions: sql.DataFrame)(implicit spark: SparkSession): DataFrame = {
     import spark.implicits._
-    val sr = DataFrameUtils.load(s"$base/sr.ndjson",
-      $"id", $"status", $"intent", $"authoredOn", $"code", $"subject", $"specimen",
+    val serviceRequest = DataFrameUtils.load(s"$base/sr.ndjson",$"id", $"status", $"intent", $"authoredOn", $"code", $"subject", $"specimen",
       //refGetter(expr("extension.valueReference.reference")) as "ci_ref")
     expr("extension[0].valueReference.reference").substr(20,7) as "ci_ref", $"requester.id" as "requester_id")
 
-    val serviceRequestWithClinicalImpression = sr.select($"id", $"status", $"intent", $"authoredOn", $"code", $"subject", $"specimen", $"ci_ref", $"requester_id")
-      .join(clinicalImpressions.select($"status" as "ci_status", $"id" as "ci_id"), $"ci_ref" === $"ci_id")
+    val serviceRequestWithClinicalImpression = serviceRequest
+      .select($"id", $"status", $"intent", $"authoredOn", $"code", $"subject", $"specimen", $"ci_ref", $"requester_id")
+      .join(clinicalImpressions.select($"status" as "ci_status", $"id" as "ci_id", $"assessor_id"), $"ci_ref" === $"ci_id")
 
-    val serviceRequestWithClinicalImpressionAndRequester = serviceRequestWithClinicalImpression.select($"id", $"status", $"intent", $"authoredOn", $"code", $"subject",
-      $"specimen", $"ci_ref", $"requester_id",$"ci_status").join(practitionerWithRoles.select($"role_id", $"name"), $"requester_id" === $"role_id")
+    val serviceRequestWithClinicalImpressionAndRequester = serviceRequestWithClinicalImpression
+      .select($"id", $"status", $"intent", $"authoredOn", $"code", $"subject", $"specimen", $"ci_ref", $"requester_id",$"ci_status")
+      .join(practitionerWithRolesAndOrg
+      .select($"role_id", $"name"), $"requester_id" === $"role_id")
 
 
     serviceRequestWithClinicalImpressionAndRequester
