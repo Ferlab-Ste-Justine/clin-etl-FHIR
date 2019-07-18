c341b6066a44e864420b8764dc2876d0885d3888
Francois Lachance <latch2112@gmail.com>
2019-07-18 16:48:18 -0400

    Update ETL, CI with item

--- src/main/scala/org/crsj/clin/etl/ClinicalImpression.scala
+++ src/main/scala/org/crsj/clin/etl/ClinicalImpression.scala
@@ -20,7 +20,7 @@ object ClinicalImpression {
     import spark.implicits._
     val clinicalImpression = DataFrameUtils.load(s"$base/ci.ndjson", $"id", $"subject",
       $"status", $"effective", $"extension.valueAge.value" (0) as "runtimePatientAge", $"assessor.id" as "assessor_id",
-      items($"investigation") as "iiu")
+      items($"investigation.item") as "iiu")
 
     val clinicalImpressionWithAssessor = clinicalImpression
       .select($"id", $"subject", $"status", $"effective" as "ci_consultation_date", $"runtimePatientAge", $"assessor_id", $"iiu")
