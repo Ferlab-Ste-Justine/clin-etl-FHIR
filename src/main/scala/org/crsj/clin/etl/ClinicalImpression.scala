9885d54817042596c830a889c5743422607f0356
Francois Lachance <latch2112@gmail.com>
2019-07-18 16:46:29 -0400

    Update ETL, CI with item

--- src/main/scala/org/crsj/clin/etl/ClinicalImpression.scala
+++ src/main/scala/org/crsj/clin/etl/ClinicalImpression.scala
@@ -1,14 +1,26 @@
 package org.crsj.clin.etl
 
 import org.apache.spark.sql
-import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.udf
+import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 
 object ClinicalImpression {
+
+  case class item(code: String)
+
+  private val items = udf((data: Seq[Row]) => {
+    if (data == null) None
+    else Some {
+      data.map { r => item(r.getAs("uri"))
+      }
+    }
+  })
+
   def load(base: String, practitionerWithRolesAndOrg: sql.DataFrame)(implicit spark: SparkSession): DataFrame = {
     import spark.implicits._
     val clinicalImpression = DataFrameUtils.load(s"$base/ci.ndjson", $"id", $"subject",
       $"status", $"effective", $"extension.valueAge.value" (0) as "runtimePatientAge", $"assessor.id" as "assessor_id",
-      $"investigation.item.uri" as "iiu")
+      items($"investigation") as "iiu")
 
     val clinicalImpressionWithAssessor = clinicalImpression
       .select($"id", $"subject", $"status", $"effective" as "ci_consultation_date", $"runtimePatientAge", $"assessor_id", $"iiu")
