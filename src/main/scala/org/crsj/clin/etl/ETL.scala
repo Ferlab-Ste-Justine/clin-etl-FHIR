bcc25df7dd5b1497d69fdb5696f16a0e987b57c8
Francois Lachance <latch2112@gmail.com>
2019-07-22 16:20:32 -0400

    Update ETL, group and studies

--- src/main/scala/org/crsj/clin/etl/ETL.scala
+++ src/main/scala/org/crsj/clin/etl/ETL.scala
@@ -1,7 +1,7 @@
 package org.crsj.clin.etl
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.expr
+import org.apache.spark.sql.functions.{explode, expr}
 import org.crsj.clin.etl.DataFrameUtils.{joinAggregateFirst, joinAggregateList}
 import org.elasticsearch.spark.sql._
 
@@ -25,7 +25,7 @@ object ETL {
         expr("array_contains(iiu.uri, fmh_id)"), "familyMemberHistory")
     val serviceRequest = ServiceRequest.load(base, practitionerWithRolesAndOrg, fullClinicalImpressionsWithObservationsAndFMH)
     val studyWithPatients = Study.load(base)
-    val group = Group.load(base)
+    val groups = Group.load(base)
 
     //val withObservations = joinAggregateList(patients, observationsWithPerformer, patients("id") === $"subject.id", "observations")
     val withPractitioners = joinAggregateList(patients, practitionerWithRolesAndOrg, expr("array_contains(generalPractitioner.id, role_id)"), "practitioners")
@@ -34,10 +34,16 @@ object ETL {
     val withOrganizations = joinAggregateFirst(withClinicalImpressions, organizations, withClinicalImpressions("managingOrganization.id") === organizations("id"), "organization")
     val withServiceRequest = joinAggregateList(withOrganizations, serviceRequest, withOrganizations("id") === $"subject.id", "serviceRequests")
     val withStudy = joinAggregateList(withServiceRequest, studyWithPatients, withServiceRequest("id") === $"patient.entity.id", "studies")
-    val withGroup = joinAggregateList(withStudy, group, withStudy("id") === $"patient.entity.id", "group")
+    val withGroup = joinAggregateList(withStudy, groups, withStudy("id") === $"patient.entity.id", "group")
     //val withFamilyMemberHistory = joinAggregateList(withStudy, familyMemberHistory, withStudy("id") === $"patient.id", "familyMemberHistory")
+    val group = DataFrameUtils.load(s"$base/group.ndjson", $"id" as "group_id", explode($"member") as "patient")
+    val study = DataFrameUtils.load(s"$base/study.ndjson", $"id" as "study_id", $"title", explode($"enrollment") as "enrollment")
 
-    withGroup.saveToEs("patient/patient", Map("es.mapping.id" -> "id"))
+    withGroup.saveToEs("temp/temp", Map("es.mapping.id" -> "id"))
+    groups.saveToEs("studies/groups", Map("es.mapping.id" -> "study_id"))
+    group.saveToEs("group/group", Map("es.mapping.id" -> "group_id"))
+    study.saveToEs("study/study", Map("es.mapping.id" -> "study_id"))
+
     //withFamilyMemberHistory.saveToEs("patient/patient", Map("es.mapping.id" -> "id"))
 
   }
--- src/main/scala/org/crsj/clin/etl/Group.scala
+++ src/main/scala/org/crsj/clin/etl/Group.scala
@@ -8,20 +8,20 @@ object Group {
   def load(base: String)(implicit spark: SparkSession): DataFrame = {
     import spark.implicits._
 
-    val group = DataFrameUtils.load(s"$base/group.ndjson", $"id" as "group_id", $"member" as "patient")
+    val group = DataFrameUtils.load(s"$base/group.ndjson", $"id" as "group_id", explode($"member") as "patient")
     val study = DataFrameUtils.load(s"$base/study.ndjson", $"id" as "study_id", $"title", explode($"enrollment") as "enrollment")
 
 
-//    val groupWithStudy = study.select($"study_id", $"title", $"enrollment")
-//      .join(group.select($"group_id", $"patient"), $"enrollment.id" === $"group_id")
-//      .select($"study_id", $"title", $"patient", $"enrollment", $"group_id")
+    val groupWithStudy = study.select($"study_id", $"title", $"enrollment")
+      .join(group.select($"group_id", $"patient"), $"enrollment.id" === $"group_id")
+      .select($"study_id", $"title", $"patient", $"enrollment", $"group_id")
 
 
-    val studiesWithGroup = joinAggregateList(study, group,
-      expr("array_contains(enrollment.id, group_id)"), "group")
+//    val studiesWithGroup = joinAggregateList(study, group,
+//      expr("array_contains(enrollment.id, group_id)"), "group")
 
 
-    //groupWithStudy
-    studiesWithGroup
+    groupWithStudy
+//    studiesWithGroup
   }
 }
