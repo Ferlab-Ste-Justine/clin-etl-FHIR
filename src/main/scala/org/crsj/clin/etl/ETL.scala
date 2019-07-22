ee8e9ac78d6e7a8d8b85194baf8ee19f998aeb86
Francois Lachance <latch2112@gmail.com>
2019-07-22 17:33:45 -0400

    Update ETL, group and studies

--- src/main/scala/org/crsj/clin/etl/ETL.scala
+++ src/main/scala/org/crsj/clin/etl/ETL.scala
@@ -37,7 +37,7 @@ object ETL {
     val withGroup = joinAggregateList(withStudy, groups, withStudy("id") === $"patient.entity.id", "group")
     //val withFamilyMemberHistory = joinAggregateList(withStudy, familyMemberHistory, withStudy("id") === $"patient.id", "familyMemberHistory")
     val group = DataFrameUtils.load(s"$base/group.ndjson", $"id" as "group_id", $"member" as "patient")
-    val study = DataFrameUtils.load(s"$base/study.ndjson", $"id" as "study_id", $"title", $"enrollment.id" as "enrollement_id")
+    val study = DataFrameUtils.load(s"$base/study.ndjson", $"id" as "study_id", $"title", $"enrollment.id" as "enrollment_id")
 
     val studyWithGroup = joinAggregateList(study, group,
       expr("array_contains(enrollment_id, group_id)"), "group")
