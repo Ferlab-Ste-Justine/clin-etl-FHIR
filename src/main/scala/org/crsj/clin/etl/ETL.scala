c6a2b1fe488731fecc040fd3340d36d1c43cff1e
Francois Lachance <latch2112@gmail.com>
2019-07-23 08:53:18 -0400

    Update ETL, group and studies

--- src/main/scala/org/crsj/clin/etl/ETL.scala
+++ src/main/scala/org/crsj/clin/etl/ETL.scala
@@ -37,14 +37,16 @@ object ETL {
     val withGroup = joinAggregateList(withStudy, groups, withStudy("id") === $"patient.entity.id", "grousps")
     //val withFamilyMemberHistory = joinAggregateList(withStudy, familyMemberHistory, withStudy("id") === $"patient.id", "familyMemberHistory")
     val group = DataFrameUtils.load(s"$base/group.ndjson", $"id" as "group_id", expr( "member.entity.id") as "patient_id")
+    //val explodedGroup = group.select("group_id", )
     val study = DataFrameUtils.load(s"$base/study.ndjson", $"id", $"id" as "study_id", $"title", expr("enrollment.id") as "enrollment_id")
-
+    val explodedStudy = study.select($"id", $"id" as "study_id", $"title", explode($"enrollment_id"))
     val studyWithGroup = joinAggregateList(study, group, expr("array_contains(enrollment_id, group_id)"), "group")
 
     withGroup.saveToEs("temp/temp", Map("es.mapping.id" -> "id"))
     groups.saveToEs("studies/groups", Map("es.mapping.id" -> "study_id"))
     group.saveToEs("group/group", Map("es.mapping.id" -> "group_id"))
     study.saveToEs("study/study", Map("es.mapping.id" -> "study_id"))
+    explodedStudy.saveToEs("exstudy/exstudy", Map("es.mapping.id" -> "study_id"))
     studyWithGroup.saveToEs("groupss/groupss", Map("es.mapping.id" -> "study_id"))
 
     //withFamilyMemberHistory.saveToEs("patient/patient", Map("es.mapping.id" -> "id"))
