d8e22dcc5aa9b5219fbd14d9477a6a666afa09aa
Francois Lachance <latch2112@gmail.com>
2020-01-22 13:33:30 -0500

    Fix organization alias

--- src/main/scala/org/crsj/clin/etl/Organization.scala
+++ src/main/scala/org/crsj/clin/etl/Organization.scala
@@ -1,10 +1,11 @@
 package org.crsj.clin.etl
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.functions.{expr, udf}
 
 object Organization {
   def load(base: String)(implicit spark: SparkSession): DataFrame = {
     import spark.implicits._
-    DataFrameUtils.load(s"$base/org.ndjson", $"id", $"name", $"alias")
+    DataFrameUtils.load(s"$base/org.ndjson", $"id", $"name", expr("alias[0]") as "alias")
   }
 }
