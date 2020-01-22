7d3d3a686662eff0b0b3b979aeadce74aacdf313
Francois Lachance <latch2112@gmail.com>
2020-01-22 16:23:36 -0500

    Fix SR to have id of the practitioner not just it's role id

--- src/main/scala/org/crsj/clin/etl/ServiceRequest.scala
+++ src/main/scala/org/crsj/clin/etl/ServiceRequest.scala
@@ -14,17 +14,17 @@ object ServiceRequest {
     import spark.implicits._
     val serviceRequest = DataFrameUtils.load(s"$base/sr.ndjson",$"id", $"status", $"intent", $"authoredOn", $"code", $"subject", $"specimen",
       //refGetter(expr("extension.valueReference.reference")) as "ci_ref")
-    expr("extension[0].valueReference.reference").substr(20,7) as "ci_ref", $"requester.id" as "requester_id")
+    expr("extension[0].valueReference.reference").substr(20,7) as "ci_ref", $"requester.id" as "requester_role_id")
 
     val serviceRequestWithClinicalImpression = serviceRequest
-      .select($"id", $"status", $"intent", $"authoredOn", $"code", $"subject", $"specimen", $"ci_ref", $"requester_id")
+      .select($"id", $"status", $"intent", $"authoredOn", $"code", $"subject", $"specimen", $"ci_ref", $"requester_role_id")
       .join(clinicalImpressions.select($"status" as "ci_status", $"id" as "ci_id", $"assessor_id"), $"ci_ref" === $"ci_id")
 
     val serviceRequestWithClinicalImpressionAndRequester = serviceRequestWithClinicalImpression
-      .select($"id", $"status", $"intent", $"authoredOn", $"code", $"subject", $"specimen", $"ci_ref", $"requester_id",$"ci_status")
+      .select($"id", $"status", $"intent", $"authoredOn", $"code", $"subject", $"specimen", $"ci_ref", $"requester_role_id",$"ci_status")
       .join(practitionerWithRolesAndOrg
-      .select($"role_id", $"name" as "requester_name", $"org_name" as "requester_org_name", $"org_alias" as "requester_org_alias",  $"org_id" as "requester_org_id"),
-        $"requester_id" === $"role_id")
+      .select($"role_id", $"name" as "requester_name", $"org_name" as "requester_org_name", $"org_alias" as "requester_org_alias",  $"org_id" as "requester_org_id", $"pract_id"),
+        $"requester_role_id" === $"role_id")
 
 
     serviceRequestWithClinicalImpressionAndRequester
