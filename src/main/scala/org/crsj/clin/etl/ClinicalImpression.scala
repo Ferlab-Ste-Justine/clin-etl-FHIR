0f4c370cbce84a3ca4a2082dbc77474234ab9593
Francois Lachance <latch2112@gmail.com>
2020-01-23 17:31:08 -0500

    Adding assessor id and role id

--- src/main/scala/org/crsj/clin/etl/ClinicalImpression.scala
+++ src/main/scala/org/crsj/clin/etl/ClinicalImpression.scala
@@ -8,13 +8,14 @@ object ClinicalImpression {
   def load(base: String, practitionerWithRolesAndOrg: sql.DataFrame)(implicit spark: SparkSession): DataFrame = {
     import spark.implicits._
     val clinicalImpression = DataFrameUtils.load(s"$base/ci.ndjson", $"id", $"subject",
-      $"status", $"effective", $"extension.valueAge.value" (0) as "runtimePatientAge", $"assessor.id" as "assessor_id",
+      $"status", $"effective", $"extension.valueAge.value" (0) as "runtimePatientAge", $"assessor.id" as "assessor_role_id",
       $"investigation.item" (0) as "iiu")
 
     val clinicalImpressionWithAssessor = clinicalImpression
-      .select($"id", $"subject", $"status", $"effective" as "ci_consultation_date", $"runtimePatientAge", $"assessor_id", $"iiu")
+      .select($"id", $"subject", $"status", $"effective" as "ci_consultation_date", $"runtimePatientAge", $"assessor_role_id", $"iiu")
       .join(practitionerWithRolesAndOrg
-      .select($"role_id", $"name" as "assessor_name", $"org_name" as "assessor_org_name", $"org_id" as "assessor_org_id"), $"assessor_id" === $"role_id")
+      .select($"role_id", $"name" as "assessor_name", $"org_name" as "assessor_org_name", $"org_id" as "assessor_org_id", $"id" as "assessor_id"),
+        $"assessor_role_id" === $"role_id")
     clinicalImpressionWithAssessor
   }
 }
