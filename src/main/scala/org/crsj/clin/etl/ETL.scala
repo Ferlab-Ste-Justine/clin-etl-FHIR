6e2827d7852d81cc04623d75e6e8d6da13189954
Francois Lachance <latch2112@gmail.com>
2020-01-29 13:31:56 -0500

    fix proper index

--- src/main/scala/org/crsj/clin/etl/ETL.scala
+++ src/main/scala/org/crsj/clin/etl/ETL.scala
@@ -32,7 +32,7 @@ object ETL {
     val withServiceRequest = joinAggregateList(withOrganizations, serviceRequest, withOrganizations("id") === $"subject.id", "serviceRequests")
     val withStudyAndGroup = joinAggregateList(withServiceRequest, studyWithGroup, withServiceRequest("id") === $"patient", "studies")
 
-    withStudyAndGroup.saveToEs("patient/_doc", Map("es.mapping.id" -> "id"))
+    withStudyAndGroup.saveToEs("patient/patient", Map("es.mapping.id" -> "id"))
 
   }
 
