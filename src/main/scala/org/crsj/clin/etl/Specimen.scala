91ac94c8102bf1d311462298121ccf5dfc5462f9
Eric Vallee <eric_vallee@webificservices.com>
2020-02-26 00:47:46 -0500

    Added two new fields to specimen in elasticsearch for overture

--- src/main/scala/org/crsj/clin/etl/Specimen.scala
+++ src/main/scala/org/crsj/clin/etl/Specimen.scala
@@ -6,6 +6,6 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 object Specimen {
   def load(base: String)(implicit spark: SparkSession): DataFrame = {
     import spark.implicits._
-    DataFrameUtils.load(s"$base/sp.ndjson", $"id", $"subject", $"status", $"request.id" as "request", expr("container.identifier[0].value") as "container")
+    DataFrameUtils.load(s"$base/sp.ndjson", $"id", $"subject", $"status", $"request.id" as "request", expr("container.identifier[0].value") as "container", $"type", expr("parent[0].id") as "parent")
   }
 }
