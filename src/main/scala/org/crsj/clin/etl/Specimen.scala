8006c22e03d0d8da8497fd6bd8ab47a1633c9566
Eric Vallee <eric_vallee@webificservices.com>
2020-02-26 19:00:05 -0500

    Progress implementing etl join between specimens and samples

--- src/main/scala/org/crsj/clin/etl/Specimen.scala
+++ src/main/scala/org/crsj/clin/etl/Specimen.scala
@@ -1,11 +1,28 @@
 package org.crsj.clin.etl
 
-import org.apache.spark.sql.functions.expr
+import org.apache.spark.sql.functions.{col, expr, struct}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object Specimen {
   def load(base: String)(implicit spark: SparkSession): DataFrame = {
     import spark.implicits._
-    DataFrameUtils.load(s"$base/sp.ndjson", $"id", $"subject", $"status", $"request.id" as "request", expr("container.identifier[0].value") as "container", $"type", expr("parent[0].id") as "parent")
+    val rawSpecimen = DataFrameUtils.load(s"$base/sp.ndjson", $"id", $"subject", $"status", $"request.id" as "request", expr("container.identifier[0].value") as "container", $"type", expr("parent[0].id") as "parent")
+    val sample = rawSpecimen.filter(col("parent").isNotNull).as("sample")
+    val specimen = rawSpecimen.filter(col("parent").isNull)
+    val explodedSample = sample
+      .join(
+          specimen.as("specimen"), 
+          $"sample.parent" === $"specimen.id", 
+          "left"
+      ).select(
+          col("sample.id"), 
+          col("sample.subject"), 
+          col("sample.status"), 
+          col("sample.request"), 
+          col("sample.container"), 
+          col("sample.type"),
+          struct(specimen("*")).as("parent")
+      )
+    (specimen, explodedSample)
   }
 }
